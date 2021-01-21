3358793a42b3411caf9e0c74e021092cb7c6f9a8
wildandhya <wildandhya15@gmail.com>
2021-01-21 23:58:19 +0700

    remove unnessery variable

--- src/page/CandlestickChart.js
+++ src/page/CandlestickChart.js
@@ -81,36 +81,6 @@ const CandlestickChart = ()=>{
     
         setSeries([{data:newdata}])
     },[dataApi])
-    const arrjing= [{
-        data: [{
-            x: new Date(1538778600000),
-            y: [6629.81, 6650.5, 6623.04, 6633.33]
-          },
-          {
-            x: new Date(1538780400000),
-            y: [6632.01, 6643.59, 6620, 6630.11]
-          },
-          {
-            x: new Date(1538782200000),
-            y: [6630.71, 6648.95, 6623.34, 6635.65]
-          },
-          {
-            x: new Date(1538784000000),
-            y: [6635.65, 6651, 6629.67, 6638.24]
-          },
-          {
-            x: new Date(1538785800000),
-            y: [6638.24, 6640, 6620, 6624.47]
-          },
-          {
-            x: new Date(1538787600000),
-            y: [6624.53, 6636.03, 6621.68, 6624.31]
-          },
-        ]
-        }]
-
-    console.log('babi',series)
-    console.log('anjing',arrjing)
     return(
         <div className="container">
             <div>
